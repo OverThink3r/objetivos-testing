ff0356b4c9a8f5400eba51af03aeb33ea7dc4719
ygarciaSTO <yamir.garcia@hotmail.com>
2023-10-12 18:45:47 -0600

    test alumnos

--- alumnos.test.js
+++ alumnos.test.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 const api = axios.create({
   baseURL: 'http://objetivostech.herokuapp.com/api'
 });
-describe('Prueba', () => {
-  test('should first', async () => {
-    const {status, statusText} = await api.get('/carreras');
+describe('Alumnos', () => {
+  test('Debería obtener información de los alumnos', async () => {
+    const {status, statusText, data} = await api.get('/alumnos');
+    expect(data).not.toBeNull()
     expect(status).toBe(200)
     expect(statusText).toBe('OK')
   })
